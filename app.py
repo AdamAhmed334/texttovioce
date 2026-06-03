2eca2bbd1a0550a7d4df528c4f82dc353d945d20
AdamAhmed334 <jdd28675@gmail.com>
2026-06-03 05:17:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,18 @@ import asyncio
 st.title("textovoice")
 text = st.text_area("اكتب النص")
 lang = st.selectbox("choose language",["عربي","انجلزي"])
+VOICES = {
+    "عربي": {
+        
+        " زارية (امرأة - السعودية)": "ar-SA-ZariyahNeural",
+        " شاكر (رجل - مصر)":         "ar-EG-ShakirNeural",
+        " سلمى (امرأة - مصر)":       "ar-EG-SalmaNeural",
+    },
+    "إنجليزي": {
+        " غاي (رجل - أمريكا)":       "en-US-GuyNeural",
+        " جيني (امرأة - أمريكا)":    "en-US-JennyNeural",
+        " رايان (رجل - بريطانيا)":   "en-GB-RyanNeural",
+        " ليبي (امرأة - بريطانيا)": "en-GB-LibbyNeural",
+    },
+}
+lang = st.selectbox("choose lang voice",list(VOICES[lang].keys()))
